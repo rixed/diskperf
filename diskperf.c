759d4ee9a599bb67ef01ddf412a04588114cf4f4
Cedric Cellier <rixed@happyleptic.org>
2013-01-30 16:05:18 +0100

    Fix copy-paste error in license header that nobody read but GL ;)

--- diskperf.c
+++ diskperf.c
@@ -2,20 +2,18 @@
 // vim:sw=4 ts=4 sts=4 expandtab
 /* Copyright 2013, SecurActive.
  *
- * This file is part of Junkie.
- *
- * Junkie is free software: you can redistribute it and/or modify
+ * Diskperf is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Junkie is distributed in the hope that it will be useful,
+ * Diskperf is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with Junkie.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Diskperf.  If not, see <http://www.gnu.org/licenses/>.
  */
 #define _FILE_OFFSET_BITS 64
 #define _GNU_SOURCE
